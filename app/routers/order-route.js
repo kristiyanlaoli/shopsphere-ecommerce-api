9f40718efa614e1e20610ec77d9787820ebd4836
Kristiyan <kristiyanlaoli@gmail.com>
2024-02-10 16:11:26 +0700

    add permission auth to order-route

--- app/routers/order-route.js
+++ app/routers/order-route.js
@@ -1,39 +1,49 @@
 import { Router } from "express";
 import prisma from "../utils/prisma.js";
+import { Permission } from "../utils/authorization.js";
+import {
+  authToken,
+  authorizePermission,
+} from "../middlewares/authTokenAndPermission.js";
 const router = Router();
 
 // Add to Order (Checkout)
-router.post("/order", async (req, res) => {
-  try {
-    const cart = await prisma.cart.findMany();
+router.post(
+  "/order",
+  authToken,
+  authorizePermission(Permission.ADD_ORDER),
+  async (req, res) => {
+    try {
+      const cart = await prisma.cart.findMany();
 
-    const total = cart.reduce((sum, item) => sum + item.total, 0);
+      const total = cart.reduce((sum, item) => sum + item.total, 0);
 
-    // Save the order details to the 'orders' table
-    const order = await prisma.order.create({
-      data: {
-        total,
-        created_at: new Date(), // Set the current timestamp
-        items: {
-          createMany: {
-            data: cart.map((item) => ({
-              product_id: item.productID,
-              quantity: item.quantity,
-              total: item.total,
-            })),
+      // Save the order details to the 'orders' table
+      const order = await prisma.order.create({
+        data: {
+          total,
+          created_at: new Date(), // Set the current timestamp
+          items: {
+            createMany: {
+              data: cart.map((item) => ({
+                product_id: item.productID,
+                quantity: item.quantity,
+                total: item.total,
+              })),
+            },
           },
         },
-      },
-    });
+      });
 
-    // Clear the cart after the order is placed
-    await prisma.cart.deleteMany({});
+      // Clear the cart after the order is placed
+      await prisma.cart.deleteMany({});
 
-    res.json({ message: "Order placed successfully", order });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Internal Server Error" });
+      res.json({ message: "Order placed successfully", order });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
   }
-});
+);
 
 export default router;
