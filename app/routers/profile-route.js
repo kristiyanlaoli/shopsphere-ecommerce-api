aeabf6e062d036552f28a16a285b7cfc0976b683
Kristiyan <kristiyanlaoli@gmail.com>
2024-02-16 14:13:32 +0700

    add email checker to update profile add password matching checker to update password

--- app/routers/profile-route.js
+++ app/routers/profile-route.js
@@ -1,7 +1,10 @@
 import { Router } from "express";
 import prisma from "../utils/prisma.js";
 import { authToken } from "../middlewares/middlewares.js";
-
+import { config } from "dotenv";
+import bcrypt from "bcrypt";
+config();
+const bcryptRound = Number(process.env.BCRYPT_ROUND);
 const router = Router();
 
 // Show profile
@@ -12,6 +15,22 @@ router.get("/profile", authToken, async (req, res) => {
 // Update profile
 router.put("/profile", authToken, async (req, res) => {
   const { name, email, image, role_id } = req.body;
+  //check if name and email is empty
+  if (!name || !email) {
+    return res.status(400).json({ message: "Name or email is required" });
+  }
+  //check if email already exists
+  const existingUser = await prisma.user.findFirst({
+    where: {
+      email,
+      id: {
+        not: req.user.id,
+      },
+    },
+  });
+  if (existingUser) {
+    return res.status(400).json({ message: "Email already in use" });
+  }
   await prisma.user.update({
     where: {
       id: req.user.id,
@@ -24,6 +43,42 @@ router.put("/profile", authToken, async (req, res) => {
     },
   });
   res.json({ message: "Profile updated" });
+});
+
+// Update password
+router.put("/profile/password", authToken, async (req, res) => {
+  const { old_password, new_password } = req.body;
+
+  // check if password exists
+  if (!old_password || !new_password) {
+    return res.status(400).json({ message: "Password is required" });
+  }
+
+  const user = await prisma.user.findUnique({
+    where: {
+      id: req.user.id,
+    },
+  });
+
+  if (!user) {
+    return res.status(401).json({ message: "Invalid user" });
+  }
+
+  const validPassword = bcrypt.compareSync(old_password, user.password);
+  if (!validPassword) {
+    return res.status(401).json({ message: "Invalid old password" });
+  }
+
+  const hashedPassword = bcrypt.hashSync(new_password, bcryptRound);
+  await prisma.user.update({
+    where: {
+      id: req.user.id,
+    },
+    data: {
+      password: hashedPassword,
+    },
+  });
+  res.json({ message: "Password updated" });
 });
 
 export default router;
